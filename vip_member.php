9f968d05bcfcad7a672953dfe053c06e2c740255
Elena Fedchenko <fedchenko.yelena@gmail.com>
2024-04-10 10:26:57 +1200

    added homepage code

--- vip_member.php
+++ vip_member.php
@@ -1,1 +1,18 @@
 <!-- A homepage -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>VIP Members Home Page</title>
+    </head>
+<body>
+    <h1>Welcome to the VIP Members Area</h1>
+    <p>Select an option below:</p>
+    <ul class="link-list">
+        <li><a href="member_add_form.php">Add New Member Form</a></li>
+        <li><a href="member_display.php">Display All Members Page</a></li>
+        <li><a href="member_search.php">Search Member Page</a></li>
+    </ul>
+</body>
+</html>
